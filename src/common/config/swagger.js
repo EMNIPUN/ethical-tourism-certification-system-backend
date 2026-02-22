84e2609c4f7d6fdd99090ba0a962c197297d4fd2
Sadeesha Sathsara <sadeeshasathsara99@gmail.com>
2026-02-22 21:16:26 +0530

    feat: Implement Swagger configuration and define authentication API routes with OpenAPI documentation.

--- src/common/config/swagger.js
+++ src/common/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
       },
       servers: [
          {
-            url: "http://localhost:5001/api/v1",
+            url: `http://localhost:${process.env.PORT || 5000}/api/v1`,
             description: "Development server",
          },
       ],
